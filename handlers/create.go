2ec1476f7a71c10dffd2eba7c94cc92c4c282093
yuriBaza23 <yuribaza2321@gmail.com>
2023-04-15 23:49:13 -0300

    feat: handler create

---
+++ handlers/create.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopi/models"
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var school models.School
+
+	err := json.NewDecoder(r.Body).Decode(&school)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	id, err := models.InsertSchool(school)
+
+	var resp map[string]any
+	if err != nil {
+		resp = map[string]any{
+			"error":   true,
+			"message": "Error inserting school",
+		}
+	} else {
+		resp = map[string]any{
+			"error":   false,
+			"message": fmt.Sprintf(`School created with ID: %d`, id),
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
