83eecd5aee0739990e579b906f8e10eb4bffdebc
yuriBaza23 <yuribaza2321@gmail.com>
2023-04-19 14:32:23 -0300

    feat: add new routes

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,13 +5,17 @@ import (
 	"gopi/internal/api/v1/handlers"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/mux"
 )
 
 func HttpInit(port string) {
-	r := chi.NewRouter()
+	r := mux.NewRouter()
 
-	r.Post("/school", handlers.CreateSchool)
+	r.HandleFunc("/school", handlers.GetAllSchool).Methods("GET")
+	r.HandleFunc("/school/{id}", handlers.GetSchool).Methods("GET")
+	r.HandleFunc("/school", handlers.CreateSchool).Methods("POST")
+	r.HandleFunc("/school/{id}", handlers.DeleteSchool).Methods("DELETE")
+	r.HandleFunc("/school/{id}", handlers.UpdateSchool).Methods("PUT")
 
 	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
